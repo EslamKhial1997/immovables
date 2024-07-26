e42a3e551b0f9cb347a89933c9346c2bad389cd4
EslamKhial1997 <eslamkhial1997@gmail.com>
2024-07-26 19:06:56 +0300

    dashboard , Auth

---
+++ .history/src/Components/auth/login/Login_20240726155211.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Profile from "../../../assets/auth/profileLogin.svg";
+import Styles from "../Styles.module.css";
+
+import { NavLink, Outlet } from "react-router-dom";
+import LoginEmail from "./LoginEmail";
+
+const Login = () => {
+  return (
+    <main className={`${Styles.main} container`}>
+      <div className="">
+        <img src={Profile} alt="" />
+      </div>
+      <div className="">
+        {" "}
+        <section className={Styles.sectionimg}>
+          <img src={Profile} alt="img" />
+        </section>
+        <section className={Styles.sectionlogin}>
+          <div className={Styles.parent}>
+            <span className={Styles.started}>اهلا بك من جديد</span>
+
+            <LoginEmail />
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
---
+++ .history/src/Components/auth/login/Login_20240726161606.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Profile from "../../../assets/logo.png";
+import auth from "../../../assets/auth/auth.png";
+import Styles from "../Styles.module.css";
+
+import { NavLink, Outlet } from "react-router-dom";
+import LoginEmail from "./LoginEmail";
+
+const Login = () => {
+  return (
+    <main className={`${Styles.main} grid grid-cols-3 sm:grid-col-1 gap-4`}>
+      <div className="grid grid-cols-1 ">
+        {" "}
+        <section className={Styles.sectionimg}>
+          <img src={Profile} alt="img" />
+        </section>
+        <section className={Styles.sectionlogin}>
+          <div className={Styles.parent}>
+            <span className={Styles.started}>اهلا بك من جديد</span>
+
+            <LoginEmail />
+          </div>
+        </section>
+      </div>
+      <div className=" col-span-2">
+        <img src={auth} alt="" className="h-screen w-screen" />
+      </div>
+    </main>
+  );
+};
+
+export default Login;
